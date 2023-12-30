9b8ed6fbef78d6b9a4a105da9ba52d083792c0b0
lexiostrov <111498664+lexiostrov@users.noreply.github.com>
2023-12-30 22:22:48 +0000

    Deploying to gh-pages from @ lexiostrov/ambarwood@d8081d95bd4259e6ebcfc0aa802f159af9c34c5a 🚀

---
+++ contacts.0412374a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".headerBtn"),t=document.querySelector(".mobMenu");e.addEventListener("click",(function(){t.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}));
+//# sourceMappingURL=contacts.0412374a.js.map
---
+++ contacts.64536156.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".headerBtn"),t=document.querySelector(".mobMenu");e.addEventListener("click",(function(){t.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=contacts.64536156.js.map
